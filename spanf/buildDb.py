c8d72644320a78124ba0aab09c97d926c51de7d4
Gleb <petrovichev.gleb@gmail.com>
2018-08-19 16:57:38 +0200

    Added imports check exception

--- spanf/buildDb.py
+++ spanf/buildDb.py
@@ -1,6 +1,7 @@
 from pony.orm import set_sql_debug
 
 from spanf.globals import db
+# noinspection PyUnresolvedReferences
 from spanf.entity import Client, Data, DataFormat, DataTransformer, EventLog, EventType, Sensor
 
 set_sql_debug(True)
