68cd5048c00ccd873dccb862606553c014570826
Gleb <petrovichev.gleb@gmail.com>
2018-08-25 15:37:24 +0200

    Writing output data to DB

--- spanf/process_data.py
+++ spanf/process_data.py
@@ -1,5 +1,7 @@
 from subprocess import Popen, PIPE
 
+from pony.orm import db_session, set_sql_debug
+
 from spanf.data_manager import DataManager
 from spanf.entities import Data, DataTransformer
 from spanf.timstamp_manager import TimestampManager
@@ -8,12 +10,22 @@ from spanf.timstamp_manager import TimestampManager
 timestampManager = TimestampManager()
 dataManager = DataManager()
 
-# set_sql_debug(True)
 
 processingTimestamp = timestampManager.updateDataProcessingTimestamp()  # Getting last timestamp and updating in to the current
 dataToProcess = dataManager.getDataNewerThan(processingTimestamp)       # Getting list of Data entities to process
 
 for data in dataToProcess:  # type: Data
     for dataTransformer in DataTransformer.getSuitable(data):  # type: DataTransformer
-        process = Popen(dataTransformer.path.split(), stdin=PIPE)
+        process = Popen(dataTransformer.path.split(), stdin=PIPE, stdout=PIPE)
         process.stdin.write(data.content)
+        out, err = process.communicate()
+
+        with db_session:
+            set_sql_debug(True)
+            Data(
+                content = out,
+                dataFormat = dataTransformer.outputDataFormat.id,
+                producer = dataTransformer.id,
+                sensor = data.sensor.id,
+                precursor = data.id
+            )
