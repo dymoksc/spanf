4639c13abf33d28115547d1868961d92e969abef
Gleb <petrovichev.gleb@gmail.com>
2018-08-26 11:13:30 +0200

    List entityName

--- web/app.py
+++ web/app.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/list/<entityName>')
+def list(entityName):
+    # type: (str) -> str
+    return render_template('layout.html', contents=entityName)
+
 @app.route('/')
-def login():
-    return render_template('layout.html')
+def index():
+    # type: () -> str
+    template = render_template('layout.html', content='index')
+    return template
+
